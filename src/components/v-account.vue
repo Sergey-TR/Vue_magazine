a17af2d64b4e609ea56cc217f15e54f5e5f1662d
Sergey-TR <s.trofimov72@gmail.com>
2020-10-26 16:53:37 +0300

    add component vue

---
+++ src/components/v-account.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="v-account"></div>
+</template>
+
+<script>
+export default {
+    name: "v-account",
+    props: {},
+    data() {
+        return {}
+    },
+    computed: {}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="v-catalog-item">
+        <p>Item1</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "v-catalog-item",
+    props: {},
+    data() {
+        return {}
+    },
+    computed: {}
+}
+</script>
+
+<style>
+
+</style>
