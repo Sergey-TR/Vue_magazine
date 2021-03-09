d01cda7570f6f00df594f255ec98dc7815e27534
Sergey-TR <s.trofimov72@gmail.com>
2021-03-09 09:17:39 +0300

    vue shop

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -1,12 +1,20 @@
-<template>
-    <div class="v-catalog">
+<template>   
+    <div class="v-catalog center">
         <h1>Catalog</h1>
-        <v-catalog-item/>
+        <div class="v-catalog-box">
+            <v-catalog-item
+                v-for="product in PRODUCTS"
+                :key="product.id"
+                :product_data="product"
+                @addToCart="addToCart"
+            />
+        </div>
     </div>
 </template>
 
 <script>
-import vCatalogItem from './v-catalog-item' 
+import vCatalogItem from './v-catalog-item'
+import {mapActions, mapGetters} from 'vuex' 
 
 export default {
     name: "v-catalog",
@@ -17,10 +25,41 @@ export default {
     data() {
         return {}
     },
-    computed: {}
+    computed: {
+        ...mapGetters([
+            'PRODUCTS',
+            'CART'
+        ]),
+    },
+    methods: {
+        ...mapActions ([
+            'GET_PRODUCTS_FROM_API',
+            'ADD_TO_CART'
+        ]),
+        addToCart(data) {
+            this.ADD_TO_CART(data);
+        }
+    },
+    mounted() {
+        this.GET_PRODUCTS_FROM_API()
+        .then((response) => {
+            if(response.data) {
+                console.log('DATA ARRIVED!')
+            }
+        })
+    }
 }
 </script>
 
 <style>
+    .v-catalog {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 
+    .center {
+        padding: 0 calc(50% - 570px);
+    }
 </style>
